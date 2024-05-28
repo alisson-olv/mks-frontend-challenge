b11fd600c788b9f2818e37ad56d79eadeb1b8c79
Alisson <binho_alisson@hotmail.com>
2024-05-27 23:11:17 -0300

    feat: creating header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,22 +3,30 @@ import React from 'react'
 import { StyledCart, StyledContainer, StyledLogo, StyledWrapper } from './styles'
 import Link from 'next/link'
 import CartIcon from '@/icons/cart-icon'
+import { useCart } from '@/context/CartContext'
+import { motion } from 'framer-motion'
 
-interface HeaderProps {
-  numberOfProducts: number;
-}
+export default function Header() {
+  const { openCart, cartItems } = useCart();
 
-export default function Header({ numberOfProducts }: HeaderProps) {
+
   return (
     <StyledWrapper>
       <StyledContainer>
         <Link href={'/'}>
           <StyledLogo>MKS <span>Sistemas</span></StyledLogo>
         </Link>
-        <StyledCart>
-          <CartIcon />
-          <span>{numberOfProducts}</span>
-        </StyledCart>
+        <motion.div
+          key={cartItems.length} // Importante para a animação ser acionada
+          initial={{ y: -10 }}
+          animate={{ y: [0, -10, 0], transition: { duration: 0.5 } }}
+          exit={{ opacity: 0, y: 10 }}
+        >
+          <StyledCart onClick={openCart}>
+            <CartIcon />
+            <span>{cartItems.length}</span>
+          </StyledCart>
+        </motion.div>
       </StyledContainer>
     </StyledWrapper>
   )
