043f1a1850c3800a545e4d0e4d8360742ecf8c7d
Alisson <binho_alisson@hotmail.com>
2024-07-10 17:18:14 -0300

    feat: adding skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-'use client'
-import CardProduct from "@/components/CardProduct";
-import Cart from "@/components/Cart";
-import SkeletonCardProduct from "@/components/SkeletonCardProduct";
-import { useCart } from "@/context/CartContext";
-import useProducts from "@/hooks/UseProducts"
+'use client';
+import CardProduct from '@/components/CardProduct';
+import Cart from '@/components/Cart';
+import SkeletonCardProduct from '@/components/SkeletonCardProduct';
+import { useCart } from '@/context/CartContext';
+import useProducts from '@/hooks/UseProducts';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -11,11 +11,16 @@ export default function Home() {
   const { products, loading, error } = useProducts();
   const { isVisible } = useCart();
 
-  if (error) return <><p>{error}</p></>
+  if (error)
+    return (
+      <>
+        <p>{error}</p>
+      </>
+    );
   return (
     <>
       <ToastContainer />
-      <main className="home">
+      <main className='home'>
         {isVisible && <Cart />}
 
         {loading && <SkeletonCardProduct cards={8} />}
--- src/components/SkeletonCardProduct/index.tsx
+++ src/components/SkeletonCardProduct/index.tsx
@@ -1,32 +1,54 @@
 import React from 'react';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css'
-import { StyledCardAndPrice, StyledCardImage, StyledCard, StyledDescription, StyledCardWrapper, StyledButton } from '../CardProduct/styles';
+import 'react-loading-skeleton/dist/skeleton.css';
+import {
+  StyledCardAndPrice,
+  StyledCardImage,
+  StyledCard,
+  StyledDescription,
+  StyledCardWrapper,
+  StyledButton,
+} from '../CardProduct/styles';
 import ShoppingBagIcon from '@/icons/shopping-bag-icon';
 
 export default function SkeletonCardProduct({ cards }: { cards: number }) {
   return (
-    <SkeletonTheme baseColor="#8b8686" highlightColor="#b5a9a9">
-      {Array(cards).fill(0).map((_, index) => (
-        <StyledCardWrapper key={index}>
-          <StyledCard>
-            <StyledCardImage>
-              <Skeleton width={190} height={180} />
-            </StyledCardImage>
-            <div style={{ display: 'flex', alignItems: 'center', gap: '20px', justifyContent: 'space-between' }}>
-              <h2><Skeleton width={100} /></h2>
-              <span><Skeleton width={50} /></span>
-            </div>
-            <StyledDescription>
-              <p><Skeleton count={3} /></p>
-            </StyledDescription>
-          </StyledCard>
-          <StyledButton>
-            <ShoppingBagIcon />
-            {/* <span>COMPRAR</span> */}
-          </StyledButton>
-        </StyledCardWrapper>
-      ))}
+    <SkeletonTheme baseColor='#8b8686' highlightColor='#b5a9a9'>
+      {Array(cards)
+        .fill(0)
+        .map((_, index) => (
+          <StyledCardWrapper key={index}>
+            <StyledCard>
+              <StyledCardImage>
+                <Skeleton width={190} height={180} />
+              </StyledCardImage>
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: '20px',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <h2>
+                  <Skeleton width={100} />
+                </h2>
+                <span>
+                  <Skeleton width={50} />
+                </span>
+              </div>
+              <StyledDescription>
+                <p>
+                  <Skeleton count={3} />
+                </p>
+              </StyledDescription>
+            </StyledCard>
+            <StyledButton>
+              <ShoppingBagIcon />
+              {/* <span>COMPRAR</span> */}
+            </StyledButton>
+          </StyledCardWrapper>
+        ))}
     </SkeletonTheme>
   );
-};
+}
